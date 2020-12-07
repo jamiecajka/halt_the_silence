97aeea0c227ba45c48c6a39e7cc1e330d15409cb
Jamie Cajka <jamiecajka@gmail.com>
2020-12-07 17:12:39 -0600

    added code for x-frame in statements controller

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class StatementsController < ApplicationController
+  after_action :allow_iframe, only: :embed
+  def embed
+  end
+
   def index
     @statements = Statement.all
   end
 
   def show
     @statement = Statement.all
+  end
+
+  private
+
+  def allow_iframe
+    response.headers.except! 'X-Frame-Options'
   end
 end
